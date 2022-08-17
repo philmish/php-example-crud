5378ae92faadb1140d1fb7ab1c107fd8b62cc781
philmish <thantalosdream@protonmail.com>
2022-08-17 23:37:58 +0200

    feat: add notes table

--- docker/mariadb/init.sql
+++ docker/mariadb/init.sql
@@ -10,5 +10,18 @@ CREATE TABLE IF NOT EXISTS Users
     PRIMARY KEY (id)
 );
 
+CREATE TABLE IF NOT EXISTS Notes
+(
+    id SERIAL,
+    created DATE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    content varchar(350) NOT NULL,
+    author_id BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES Users (id),
+    PRIMARY KEY(id)
+);
+
 INSERT INTO Users (email, password, name)
 VALUES ("test@mail.com", "$1$jwGbpR.A$zYCjQtuXUwR.aauJuU8He0", "test");
+
+INSERT INTO Notes (content, author_id)
+VALUES ("This is a test note", 1), ("This is a second test note", 1);
