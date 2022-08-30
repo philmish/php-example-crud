619122cb4bf980c9aacc6d5cada804e75ccdcd75
philmish <thantalosdream@protonmail.com>
2022-08-30 23:12:44 +0200

    feat: implement create constructor for Notes

--- src/api/services/notes/NoteModel.php
+++ src/api/services/notes/NoteModel.php
@@ -5,6 +5,7 @@ namespace mvcex\api\services\notes;
 use Exception;
 use mvcex\api\lib\APIModel;
 use mvcex\api\lib\APIResponse;
+use mvcex\api\lib\validation\Validator;
 use mvcex\core\Database;
 
 final class NoteModel extends APIModel {
@@ -13,15 +14,44 @@ final class NoteModel extends APIModel {
     private string $content;
     private string $created;
 
-    public function __constructor(int $id, string $created, string $content, int $authorId) {
+    public function __constructor(int $id, string $created, string $content, int $topicId) {
         $this->id = $id;
         $this->created = $created;
         $this->content = $content;
-        $this->topic_id = $authorId; 
+        $this->topic_id = $topicId; 
     }
 
     public function getContent(): string {
         return $this->content;
+    }
+
+    static public function Create(Database $db, ?array $requestData): self|Exception {
+        $rules = [
+            "content" => "required",
+            "topic_id" => "required"
+        ];
+        $validator = new Validator($rules);
+        $errors = $validator->run($requestData);
+        if (!empty($errors)) {
+            return new Exception("Invalid input");
+        }
+        $stmt = "INSERT (content, topic_id) INTO Notes VALUES (:content, :topic_id);";
+        $inserted = $db->insertOne($stmt, $requestData);
+        if (!$inserted) {
+            return new Exception("Failed to connect to database");
+        }
+        $stmt = "SELECT * FROM Notes WHERE id=?";
+        $args = [$inserted];
+        $note = $db->row($stmt, $args);
+        if (!$note) {
+            return new Exception("Failed to fetch note after creation");
+        }
+        return new self(
+            (int)$note['id'],
+            $note['created'],
+            $note['content'],
+            (int)$note['topic_id']
+        );
     }
 
     static public function Read(Database $db, ?array $requestData): self|Exception {
