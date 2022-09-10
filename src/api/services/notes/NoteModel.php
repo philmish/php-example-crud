23a672b960320c10ee76d6cabba5db68b30ed0e3
philmish <thantalosdream@protonmail.com>
2022-09-11 01:47:51 +0200

    refactor: implement ApiExceptions on NoteModel

--- src/api/services/notes/NoteModel.php
+++ src/api/services/notes/NoteModel.php
@@ -5,12 +5,15 @@ namespace mvcex\api\services\notes;
 use Exception;
 use mvcex\api\lib\APIModel;
 use mvcex\api\lib\APIResponse;
+use mvcex\api\lib\exceptions\ApiException;
+use mvcex\api\lib\exceptions\DBException;
+use mvcex\api\lib\exceptions\InvalidInputs;
+use mvcex\api\lib\exceptions\NotFound;
 use mvcex\api\lib\validation\Validator;
 use mvcex\core\Database;
 
 final class NoteModel extends APIModel {
     private int $id;
-    private int $topic_id;
     private string $content;
     private string $created;
 
@@ -25,26 +28,26 @@ final class NoteModel extends APIModel {
         return $this->content;
     }
 
-    static public function Create(Database $db, ?array $requestData): self|Exception {
+    static public function Create(Database $db, ?array $requestData): self|ApiException {
         $rules = [
             "content" => "required",
             "topic_id" => "required"
         ];
         $validator = new Validator($rules);
         $errors = $validator->run($requestData);
-        if (!empty($errors)) {
-            return new Exception("Invalid input");
+        if ($errors instanceof InvalidInputs) {
+            return $errors;
         }
         $stmt = "INSERT (content, topic_id) INTO Notes VALUES (:content, :topic_id);";
         $inserted = $db->insertOne($stmt, $requestData);
         if (!$inserted) {
-            return new Exception("Failed to connect to database");
+            return new DBException();
         }
         $stmt = "SELECT * FROM Notes WHERE id=?";
         $args = [$inserted];
         $note = $db->row($stmt, $args);
         if (!$note) {
-            return new Exception("Failed to fetch note after creation");
+            return new DBException();
         }
         return new self(
             (int)$note['id'],
@@ -54,15 +57,28 @@ final class NoteModel extends APIModel {
         );
     }
 
-    static public function Read(Database $db, ?array $requestData): self|Exception {
-        if (!array_key_exists("id", $requestData) || !array_key_exists("topic_id", $requestData)) {
-            return new Exception("Invalid input");
+    static public function Read(Database $db, ?array $requestData): self|ApiException {
+        if (!$requestData) {
+            return new InvalidInputs("Missing inputs");
+        }
+        $rules = [
+            "id" => "required",
+            "topic_id" => "required"
+        ];
+        $v = new Validator($rules);
+        $r = $v->run($requestData);
+        if ($r instanceof InvalidInputs) {
+            return $r;
         }
         $stmt = "SELECT id, created, content, topic_id FROM Notes WHERE id = ? AND topic_id = ?";
         $args = [$requestData["id"], $requestData["topic_id"]];
-        $note = $db->row($stmt, $args);
+        try {
+            $note = $db->row($stmt, $args);
+        } catch (Exception $e) {
+            return new DBException("Failed to fetch notes", null, $e);
+        }
         if (!$note) {
-            return new Exception("Note not found");
+            return new NotFound("Note not found");
         }
         return new self($note["id"], $note["created"], $note["content"], $note["topic_id"]);
 
