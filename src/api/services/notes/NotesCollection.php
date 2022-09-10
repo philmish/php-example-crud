8954c3f1436ddd0cdf9cce8e112021e5ad46109b
philmish <thantalosdream@protonmail.com>
2022-09-11 01:45:29 +0200

    refactor: implement custom exceptions on NotesCollection Model

--- src/api/services/notes/NotesCollection.php
+++ src/api/services/notes/NotesCollection.php
@@ -2,9 +2,12 @@
 
 namespace mvcex\api\services\notes;
 
-use Exception;
 use mvcex\api\lib\APIModelCollection;
 use mvcex\api\lib\APIResponse;
+use mvcex\api\lib\validation\Validator;
+use mvcex\api\lib\exceptions\ApiException;
+use mvcex\api\lib\exceptions\InvalidInputs;
+use mvcex\api\lib\exceptions\NotFound;
 use mvcex\core\Database;
 
 final class NotesCollection extends APIModelCollection {
@@ -14,20 +17,28 @@ final class NotesCollection extends APIModelCollection {
         $this->items = $items;
     }
 
-    public static function Read(Database $db, ?array $requestData): self| Exception {
-        if (!is_array($requestData) || !array_key_exists("topic_id", $requestData)) {
-            return new Exception("Invalid input");
+    public static function Read(Database $db, ?array $requestData): self|ApiException {
+        if (!$requestData) {
+            return new InvalidInputs("Missing Data");
+        }
+        $rules = [
+            "topic_id" => "required",
+        ];
+        $validator = new Validator($rules);
+        $errors = $validator->run($requestData);
+        if ($errors instanceof InvalidInputs) {
+            return $errors;
         }
         $stmt = "SELECT id, created, content, topic_id FROM Notes WHERE topic_id = ?";
         $args = [$requestData["topic_id"]];
         $collection = $db->rows($stmt, $args);
         if (!$collection) {
-            return new Exception("Notess not found");
+            return new NotFound("No notes found.");
         }
         return new self($collection);
     }
 
     public function toResponse(): APIResponse {
-       return new APIResponse(503, ["Not implemented"]);
+        return new APIResponse(200, [], $this->items);
     }
 }
