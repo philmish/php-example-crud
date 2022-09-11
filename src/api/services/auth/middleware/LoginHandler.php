21b5f3dc79fd1fea67d3bd78f12c53d74ba0cc55
philmish <thantalosdream@protonmail.com>
2022-09-11 03:01:07 +0200

    feat: add run method for LoginHandler

--- src/api/services/auth/middleware/LoginHandler.php
+++ src/api/services/auth/middleware/LoginHandler.php
@@ -3,20 +3,22 @@
 namespace mvcex\api\services\auth\middleware;
 
 use Exception;
+use mvcex\api\lib\exceptions\ApiException;
 use mvcex\api\lib\exceptions\DBException;
 use mvcex\api\lib\exceptions\InvalidInputs;
+use mvcex\api\lib\exceptions\InvalidCredentials;
 use mvcex\api\lib\middleware\MiddlewareHandler;
 use mvcex\api\lib\middleware\WaresContext;
 use mvcex\core\Database;
 
 final class LoginHandler implements MiddlewareHandler {
 
-    private function getHash(string $email, Database $db): string|false|DBException {
+    private function getHash(string $email, Database $db): string|InvalidCredentials|DBException {
         $stmt = "SELECT password FROM Users WHERE email = ?";
         try {
             $user = $db->row($stmt, [$email]);
             if (!$user) {
-                return false;
+                return new InvalidCredentials("Invalid Credentials");
             }
         } catch (Exception $e) {
             return new DBException("Something went wrong while logging in.", null, $e);
@@ -28,18 +30,27 @@ final class LoginHandler implements MiddlewareHandler {
         $db = $ctx->getDB();
         if (!$db) {
             $ctx->setErr(new DBException("Something went wrong loging in."));
-            $ctx->done = true;
             return $ctx;
         } 
-        $data = $ctx->getData();
-        if (!$data) {
-            $ctx->done = true;
-            $ctx->setErr(new InvalidInputs("Missing email and password"));
+        $rules = [
+            "email" => "required",
+            "password" => "required"
+        ];
+        $ctx->validateData($rules);
+        if ($ctx->done) {
             return $ctx;
-        } else if (!array_key_exists("email", $data)) {
-            $ctx->done = true;
-            $ctx->setErr(new InvalidInputs("Missing email"));
+        }
+        $hash = $this->getHash($ctx->getData()["email"], $db);
+        if ($hash instanceof ApiException) {
+            $ctx->setErr($hash);
             return $ctx;
-        } 
+        }
+        if (!password_verify($ctx->getData()["password"], $hash)) {
+            $err = new InvalidCredentials("Invalid Credentials");
+            $ctx->setErr($err);
+            return $ctx;
+        }
+        $ctx->setData(["status" => "success"]);
+        return $ctx;
     }
 }
