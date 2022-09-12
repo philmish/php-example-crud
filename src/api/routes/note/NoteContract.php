985774211eb1a963e31c9e1e7d03554df0952676
philmish <thantalosdream@protonmail.com>
2022-09-13 00:50:10 +0200

    feat: add post handler method to note route contract

--- src/api/routes/note/NoteContract.php
+++ src/api/routes/note/NoteContract.php
@@ -2,7 +2,6 @@
 
 namespace mvcex\api\routes;
 
-use Exception;
 use mvcex\api\services\notes\NotesController;
 use mvcex\core\RouteContract;
 use mvcex\api\lib\Command;
@@ -12,18 +11,16 @@ final class NoteContract extends RouteContract {
      * CRUD related to single notes
      */
     protected function GET(): void {
-        try {
-            $controller = NotesController::fromEnv();
-            $response = $controller->execute(Command::FETCH_NOTE);
-            $response->send();
-        } catch (Exception $e) {
-            echo json_encode(["error" => $e->getMessage()]);
-            http_response_code(503);
-        }
+        $controller = NotesController::fromEnv();
+        $response = $controller->execute(Command::FETCH_NOTE);
+        $response->send();
+            
     }
 
     protected function POST(): void {
-        //TODO implement routes
+        $controller = NotesController::fromEnv();
+        $response = $controller->execute(Command::CREATE_NOTE);
+        $response->send();
     }
 }
 
