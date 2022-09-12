b3d52dc72b892358df37b4435fbac6475e9b0455
philmish <thantalosdream@protonmail.com>
2022-09-12 17:39:05 +0200

    feat: add testCase for use with WaresChain to Loginhandler integration test

--- tests/integration/LoginHandlerTest.php
+++ tests/integration/LoginHandlerTest.php
@@ -1,5 +1,6 @@
 <?php declare(strict_types=1);
 
+use mvcex\api\lib\middleware\WaresChain;
 use PHPUnit\Framework\TestCase;
 use mvcex\core\Database;
 use mvcex\api\services\auth\middleware\LoginHandler;
@@ -31,6 +32,14 @@ final class LoginHandlerTest extends TestCase {
         $ctx = $handler->run($ctx);
         $this->assertFalse($ctx->done);
         $response = $ctx->toResponse();
+        $this->assertTrue($response->status === 200);
+    }
+
+    public function testLoginSuccessInChain(): void {
+        $handler = new LoginHandler();
+        $data = ["email" => "test@test.de", "password" => "Password123"];
+        $chain = new WaresChain($data, [$handler], $this->successStub);
+        $response = $chain->runChain();
         $this->assertTrue($response->status === 200);
     }
 }
