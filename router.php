17bf6fd1893d311c118c0fd85f7acd3df26a5d3a
philmish <thantalosdream@protonmail.com>
2022-09-13 00:52:05 +0200

    feat: add route for fetching notes by topic id to router

--- router.php
+++ router.php
@@ -7,14 +7,16 @@ use Exception;
 use mvcex\api\routes\LoginContract;
 use mvcex\api\routes\NotFoundContract;
 use mvcex\api\routes\NoteContract;
+use mvcex\api\routes\TopicNotesContract;
 
 $uri = rtrim($_SERVER['REQUEST_URI'], '/');
 $path = explode('?', $uri)[0];
 
 try {
-    $route = match ($uri) {
+    $route = match ($path) {
     '/login' => new LoginContract(),
     '/note' => new NoteContract(),
+    '/note/topic' => new TopicNotesContract(),
     default => new NotFoundContract(),
 }; $route->serve();
 } catch (Exception $e) {
