9d4e9016fde027aa2ebeec4a80c3f870cfdfc34e
philmish <thantalosdream@protonmail.com>
2022-09-13 00:48:25 +0200

    feat: add commands for note creation + new handlers for fetching to note controller

--- src/api/services/notes/NotesController.php
+++ src/api/services/notes/NotesController.php
@@ -2,31 +2,49 @@
 
 namespace mvcex\api\services\notes;
 
-use Exception;
-use mvcex\api\lib\APIController;
 use mvcex\api\lib\Command;
 use mvcex\api\lib\APIResponse;
-use mvcex\api\lib\exceptions\ApiException;
+use mvcex\api\lib\DBConnector;
+use mvcex\api\lib\APIController;
+use mvcex\api\lib\handlers\GetHandler;
+use mvcex\api\lib\handlers\PostHandler;
+use mvcex\api\lib\middleware\WaresChain;
+use mvcex\api\services\notes\handlers\FetchNote;
+use mvcex\api\services\notes\handlers\CreateNote;
+use mvcex\api\services\notes\handlers\FetchTopicNotes;
 
 final class NotesController extends APIController {
 
     private function getNote(): APIResponse {
-        $note = NoteModel::Read($this->db, $_GET);
-        return 
+        $handlers = [new GetHandler(), new FetchNote()];
+        $chain = new WaresChain(null, $handlers, $this->db);
+        $result = $chain->runChain();
+        return $result;
+    }
+
+    private function createNote(): APIResponse {
+        $handlers = [new PostHandler(), new CreateNote()];
+        $chain = new WaresChain(null, $handlers, $this->db);
+        $result = $chain->runChain();
+        return $result;
     }
 
     private function getNotes(): APIResponse {
-        $notes = NotesCollection::Read($this->db, $_GET);
-        if ($notes instanceof Exception) {
-            return new APIResponse(400, [$notes->getMessage()]);
-        }
-        return $notes;
+        $handlers = [new GetHandler(), new FetchTopicNotes()];
+        $chain = new WaresChain(null, $handlers, $this->db);
+        $result = $chain->runChain();
+        return $result;
+    }
+
+    static public function fromEnv(): APIController {
+        return new self(DBConnector::fromEnv());
     }
 
     public function execute(?Command $cmd): APIResponse {
         $result = match ($cmd) {
             Command::FETCH_NOTES => $this->getNotes(),
             Command::FETCH_NOTE => $this->getNote(),
+            Command::CREATE_NOTE => $this->createNote(),
             default => new APIResponse(400, ["Unknown command"]),
         };
         return $result;
